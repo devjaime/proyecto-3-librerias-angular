47f56dae89ff76d10d4f249f00f90eef4bece8b3
Jaime Hernandez <hernandez.hs@gmail.com>
2019-10-08 07:05:00 -0300

    libreria

---
+++ projects/user-widget/src/public-api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of user-widget
+ */
+export * from './lib/services/buscador-usuario.service';
+export * from './lib/user-widget.module';
+export * from './lib/interfaces/usuario.interface';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { UserWidgetModule } from '../../projects/user-widget/src/lib/user-widget.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    UserWidgetModule
   ],
   providers: [],
   bootstrap: [AppComponent]
